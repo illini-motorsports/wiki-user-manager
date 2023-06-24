616e50c1f5f13513e6802466f86bca5f2bf5fd80
Collin Meyer <collinmmeyer@gmail.com>
2023-06-24 14:55:55 -0400

    init repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css'
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <div className="App">
+        <header className="App-header">
+        </header>
+        <div className='columns'>
+          <div className='column is-2'></div>
+          <div className='column is-4 box'>
+            <p className="box is-narrow" id="p1" draggable="true">This element is draggable.</p>
+          </div>
+          <div className='column is-4 box'>
+            <p className="box is-narrow" id="p1" draggable="true">This element is draggable.</p>
+          </div>
+          <div className='column is-2'></div>
+        </div>
+      </div>
     </div>
   );
 }
